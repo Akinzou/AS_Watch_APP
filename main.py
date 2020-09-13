392565698bb140c8ce0062b23cdd27e86dd97919
Akinzou <wiktorjn@gmail.com>
2020-09-13 21:32:05 +0200

    Delete crashing app when sth is connected

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ dlg = uic.loadUi("GUI/AS_Watch_APP.ui")
 dlg.setWindowIcon(QtGui.QIcon('GUI/icon.png'))
 
 
-def ports():
+def checkPorts():
     ports = list(serial.tools.list_ports.comports())
+    dlg.PortList.clear()
 
     if ports != []:
         for p in ports:
-            dlg.PortList.addItem(p)
+            dlg.PortList.addItem(str(p))
 
         dlg.PortList.setEnabled(True)
         dlg.ActualiseTime.setEnabled(True)
@@ -23,7 +24,7 @@ def ports():
         dlg.ActualiseTime.setEnabled(False)
         dlg.Execute.setEnabled(False)
 
-dlg.Refresh.clicked.connect(ports)
-ports()
+dlg.Refresh.clicked.connect(checkPorts)
+checkPorts()
 dlg.show()
 app.exec()
